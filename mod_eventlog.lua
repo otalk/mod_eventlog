3807ca84a57d1fa31500f5cdf9f7139be934a972
Lance Stout <lancestout@gmail.com>
2014-09-19 11:22:52 -0700

    Need to load disco module first

--- mod_eventlog.lua
+++ mod_eventlog.lua
@@ -3,5 +3,8 @@ if module:get_host_type() ~= "component" then
 end
 
 
+module:depends("disco");
 module:add_identity("component", "log", module:get_option_string("name", "Event Logger"));
 module:add_feature("urn:xmpp:eventlog");
+
+module:log("info", "Started event log component");
