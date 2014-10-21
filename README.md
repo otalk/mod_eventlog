2ced893f925cacd38b9ead53d98cb8f1985487cf
Lance Stout <lancestout@gmail.com>
2014-10-21 12:58:12 -0700

    Parse out eventlog data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# `mod_eventlog`
+# mod\_eventlog
 
 Receives XEP-0337 event log data and converts & forwards it to our centralized logging facilities.
 
@@ -9,3 +9,9 @@ Add this as a component entry to your `prosody.cfg`:
 ```lua
 Component "metrics.HOSTNAME" "eventlog"
 ```
+
+## Using
+
+Event log messages with `id="log"` will be converted and logged directly to Prosody's logging system.
+
+Logs with `id="metric"` will trigger `eventlog-stat` events, one for each tag included in the log, and the event payload is an object with `room`, `metric`, and `value` fields.
--- mod_eventlog.lua
+++ mod_eventlog.lua
@@ -2,9 +2,52 @@ if module:get_host_type() ~= "component" then
     error("Event logger should be loaded as a component, please see http://prosody.im/doc/components", 0);
 end
 
+local xmlns_log = "urn:xmpp:eventlog";
+local log_levels = {
+    Debug = "debug",
+    Informational = "info",
+    Warning = "warn",
+    Error = "error"
+};
+
 
 module:depends("disco");
 module:add_identity("component", "log", module:get_option_string("name", "Event Logger"));
 module:add_feature("urn:xmpp:eventlog");
+
+
+module:hook("message/host", function (event)
+    local origin, stanza = event.origin, event.stanza;
+
+    local log = stanza:get_child("log", xmlns_log);
+    if not log then
+        return
+    end
+
+    local logType = log.attr.id;
+
+    if logType == "metric" then
+        for tag in log:childtags("tag", xmlns_log) do
+            local metric = tag.attr.name;
+            local value = tag.attr.value;
+
+            module:log("debug", "Event stat: %s = %s", metric, value);
+            module:fire_event("eventlog-stat", {
+                room = log.attr.subject,
+                metric = metric,
+                value = value
+            });
+        end
+    elseif logType == "log" then
+        local level = log_levels[log.attr.type] or "info";
+        local message = log:get_child_text("message", xmlns_log);
+        if not message then
+            return;
+        end
+
+        module:log(level, "%s", message);
+    end
+end);
+
 
 module:log("info", "Started event log component");
