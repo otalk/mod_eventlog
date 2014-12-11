270f946e666386c57cb756942e55c6276faaf22e
Lance Stout <lancestout@gmail.com>
2014-12-11 15:40:03 -0800

    Import json

--- mod_eventlog.lua
+++ mod_eventlog.lua
@@ -1,3 +1,5 @@
+local json = require "util.json";
+
 if module:get_host_type() ~= "component" then
     error("Event logger should be loaded as a component, please see http://prosody.im/doc/components", 0);
 end
